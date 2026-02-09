ff2e3aa8c0d639d81c03c2bd39b32d6616dc6f78
mcrukshan <mcrukshan75@gmail.com>
2026-02-09 12:31:38 +1100

    core: data base url configuration changed

--- services/api-gateway/config.py
+++ services/api-gateway/config.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
+from pydantic import Field
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_ignore_emty=True, extra="ignore"
-    )
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    SQLALCHEMY_DATABASE_URI : str = ""
-    SQLALCHEMY_ECHO: str = ""
+    SQLALCHEMY_DATABASE_URI: str = Field(alias="DATABASE_URL")
+    SQLALCHEMY_ECHO: bool = False
 
 
 settings = Settings()
--- services/api-gateway/main.py
+++ services/api-gateway/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="HireSight API Gateway", version="0.1.0")
 
