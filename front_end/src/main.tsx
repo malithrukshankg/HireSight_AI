89f1cc3613b7e530a95ab84884e16a0cde71632c
mcrukshan <mcrukshan75@gmail.com>
2026-02-17 21:35:10 +1100

    core: React template added

---
+++ front_end/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Auth0Provider } from "@auth0/auth0-react";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Auth0Provider
+      domain="dev-wt0zci4ynfbqzxos.us.auth0.com"
+      clientId="LpxnYgirAye57HxI2yAYpLJdEt5BauI8"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>,
+)
