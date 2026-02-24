f6f70b13ae5f78a75db898bf6feaad7f2e7adeb4
mcrukshan <mcrukshan75@gmail.com>
2026-02-24 11:38:16 +1100

    feat: Migration added for candiate table

--- services/api-gateway/models/__init__.py
+++ services/api-gateway/models/__init__.py
@@ -2,4 +2,5 @@ from .user import User
 from .organizations import Organization
 from .recruiter_organization import RecruiterOrganization
 from .jobs import Job, JobStatusEnum
+from .candidate import Candidate
 
