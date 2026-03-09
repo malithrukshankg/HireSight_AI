d2cdb1aa3719a2e0a0175ca6b3de4da2f7234950
mcrukshan <mcrukshan75@gmail.com>
2026-03-09 12:41:35 +1100

    feat: refactor api service config file

--- services/api-gateway/config.py
+++ services/api-gateway/config.py
@@ -6,11 +6,7 @@ class Settings(BaseSettings):
 
     SQLALCHEMY_DATABASE_URI: str = Field(alias="DATABASE_URL")
     SQLALCHEMY_ECHO: bool = False
-    S3_BUCKET: str = "hiresight-upload-cv"
-    AWS_REGION: str = "ap-southeast-2"
-    CV_MAX_SIZE_MB: int = 5
-    CV_PLACEHOLDER_CANDIDATE_ID: str = "00000000-0000-0000-0000-000000000001"
-    CV_PLACEHOLDER_UPLOADED_BY_USER_ID: str = "00000000-0000-0000-0000-000000000002"
+    CV_SERVICE_URL: str = "http://localhost:8001"
     CORS_ORIGINS: str = "http://localhost:3000,http://localhost:5173"
     REDIS_ENABLED: bool = True
     REDIS_URL: str = "redis://localhost:6379/0"
